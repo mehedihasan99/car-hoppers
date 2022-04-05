bfcc4b8c7ae4a47ac549562410ea46651cd3e516
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-04-05 06:58:10 +0600

    first commit

---
+++ src/components/DashBoard/DashBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashBoard = () => {
+    return (
+        <div>
+            <h2>This is dashBoard</h2>
+        </div>
+    );
+};
+
+export default DashBoard;
