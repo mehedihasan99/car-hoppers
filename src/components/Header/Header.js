e5265c4d6f80116de34803850d290e06fc9a670c
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-04-05 15:35:49 +0600

    added a custom link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css'
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import CustomLink from '../CustomLink/CustomLink';
 const Header = () => {
     return (
         <Navbar expand="lg" className='py-3 navBar'>
@@ -10,10 +11,10 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to='/home' className='text-white'>Home</Nav.Link>
-                        <Nav.Link as={Link} to='/reviews' className='text-white'>Reviews</Nav.Link>
-                        <Nav.Link as={Link} to='/dashBoard' className='text-white'>DashBoard</Nav.Link>
-                        <Nav.Link as={Link} to='/blogs' className='text-white'>Blogs</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/home' className='text-white'>Home</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/reviews' className='text-white'>Reviews</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/dashBoard' className='text-white'>DashBoard</Nav.Link>
+                        <Nav.Link as={CustomLink} to='/blogs' className='text-white'>Blogs</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
