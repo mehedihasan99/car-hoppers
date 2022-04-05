8219620618321eaf16ec6f07cd2056927952af0b
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-04-05 07:17:44 +0600

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './components/Blogs/Blogs';
 import DashBoard from './components/DashBoard/DashBoard';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={<Home></Home>} />
+        <Route path='/home' element={<Home></Home>} />
         <Route path='reviews' element={<Reviews></Reviews>} />
         <Route path='dashBoard' element={<DashBoard></DashBoard>} />
         <Route path='blogs' element={<Blogs></Blogs>} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to='/'>Car Hoppers</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+                        <Nav.Link as={Link} to='/reviews'>Reviews</Nav.Link>
+                        <Nav.Link as={Link} to='/dashBoard'>DashBoard</Nav.Link>
+                        <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
