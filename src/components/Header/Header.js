d921c2afdefde43cc486744ae76269b864f4131f
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-04-05 08:27:11 +0600

    add home page and hook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import './Header.css'
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg" className='py-3 navBar'>
             <Container>
-                <Navbar.Brand as={Link} to='/'>Car Hoppers</Navbar.Brand>
+                <Navbar.Brand as={Link} to='/' className='text-white fs-4'>Car Hoppers</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to='/home'>Home</Nav.Link>
-                        <Nav.Link as={Link} to='/reviews'>Reviews</Nav.Link>
-                        <Nav.Link as={Link} to='/dashBoard'>DashBoard</Nav.Link>
-                        <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
+                        <Nav.Link as={Link} to='/home' className='text-white'>Home</Nav.Link>
+                        <Nav.Link as={Link} to='/reviews' className='text-white'>Reviews</Nav.Link>
+                        <Nav.Link as={Link} to='/dashBoard' className='text-white'>DashBoard</Nav.Link>
+                        <Nav.Link as={Link} to='/blogs' className='text-white'>Blogs</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,29 @@
 import React from 'react';
-
+import { Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Home.css'
 const Home = () => {
     return (
         <div>
+            <Container >
+                <div className="car-container">
+                    <div className="car-text">
+                        <h2 className='text-uppercase'>Find <span style={{ color: '#004B94' }}>the Car</span> You Want</h2>
+                        <h2 className='text-uppercase'>Your Way</h2>
+                        <p className='mt-4 fs-5'>You’ll discover in-depth resources and guidance that’ll help you navigate every step of your car buying process.</p>
+                        <Button className='text-white car-btn mt-3 px-3 py-2'>Click Here</Button>
+                    </div>
+                    <div className="car-img">
+                        <img src='car.jpg' alt="" />
+                    </div>
+                </div>
+                <div className="homePage-customer-reviews text-center mt-3">
+                    <h2>Customer Reviews</h2>
+                </div>
+                <div className='reviews-btn mb-3'>
+                    <Link className='btn' to='/reviews'>See All Reviews</Link>
+                </div>
+            </Container>
         </div>
     );
 };
